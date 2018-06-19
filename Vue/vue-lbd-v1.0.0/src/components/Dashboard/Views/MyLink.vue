f1048589b0c7d06315854d692c4771e5c2b72e83
ShipJH <ship_reappearance@naver.com>
2018-06-20 00:00:53 +0900

    submit

--- Vue/vue-lbd-v1.0.0/src/components/Dashboard/Views/MyLink.vue
+++ Vue/vue-lbd-v1.0.0/src/components/Dashboard/Views/MyLink.vue
@@ -1,17 +1,20 @@
 <template>
   <div class="content">
     <div class="container-fluid">
-      <div class="row">
+      <div class="">
         
         <div id="app1" class="typo-line">
               <h1>호출성공</h1>
         </div>
         <button v-on:click="searchTerm">글 불러오기</button>
+        <button v-on:click="searchTerm2">데이터 전송</button>
 
         <div v-for="(list, index) in boardList" :key="list.boardNo">
             <div><p> {{ index }} </p></div>
             <div><p>{{list.title}}</p></div>
         </div>
+
+        <input type="text" v-model="myName"/>
       </div>
     </div>
   </div>
@@ -23,12 +26,28 @@
     data: function () {
         return {
             boardList: []
+            ,myName:''
         }
+        
     },
     methods: {
         searchTerm: function () {
             //this.linkMsg = "bye"
         this.$http.get("http://localhost:8080/board/4010/")
+            .then((response)  =>  {
+                this.boardList = response.data.boardList;
+                //this.loading = false;
+                //this.linkMsg = response.data.testValue;
+                
+            }, (error)  =>  {
+                this.loading = false;
+            })
+
+          
+        },
+
+        searchTerm2: function () {
+        this.$http.post("http://localhost:8080/board/4020/", {myName:this.myName} )
             .then((response)  =>  {
                 this.boardList = response.data.boardList;
                 //this.loading = false;
