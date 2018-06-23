7017a9a6c664be2e40db1883cb6dca63fe81385b
ShipJH <ship_reappearance@naver.com>
2018-06-23 17:12:24 +0900

    ..

--- Vue/vue-lbd-v1.0.0/src/components/Dashboard/Views/MyLink.vue
+++ Vue/vue-lbd-v1.0.0/src/components/Dashboard/Views/MyLink.vue
@@ -3,18 +3,25 @@
     <div class="container-fluid">
       <div class="">
         
-        <div id="app1" class="typo-line">
-              <h1>호출성공</h1>
-        </div>
-        <button v-on:click="searchTerm">글 불러오기</button>
-        <button v-on:click="searchTerm2">데이터 전송</button>
-
-        <div v-for="(list, index) in boardList" :key="list.boardNo">
-            <div><p> {{ index }} </p></div>
-            <div><p>{{list.title}}</p></div>
+        <div id="app1" class="">
+              <h1>게시판</h1>
         </div>
 
-        <input type="text" v-model="inputMyName"/>
+        <table>
+            <tr>
+                <th>No</th>
+                <th>제목</th>
+                <th>등록자</th>
+                <th>등록일자</th>
+            </tr>
+            <tr v-for="(list, index) in boardList" :key="list.boardNo">
+                <td>{{ index + 1 }}</td>
+                <td><a v-on:click="goDetail(list.boardNo)">{{ list.title }}</a></td>
+                <td>{{ list.regUser }}</td>
+                <td>{{ list.regDate }}</td>
+            </tr>
+        </table>
+        <!-- <input type="text" v-model="inputMyName"/> -->
       </div>
     </div>
   </div>
@@ -26,12 +33,15 @@
     data: function () {
         return {
             boardList: []
-            ,inputMyName:''
+            // ,inputMyName:''
         }
         
     },
+    created: function(){
+            this.findByBoardList();
+    },
     methods: {
-        searchTerm: function () {
+        findByBoardList: function () {
             //this.linkMsg = "bye"
         this.$http.get("http://localhost:8080/board/4010/")
             .then((response)  =>  {
@@ -44,20 +54,11 @@
             })
 
           
-        },
-
-        searchTerm2: function () {
-        this.$http.post("http://localhost:8080/board/4020/", {myName:this.inputMyName} )
-            .then((response)  =>  {
-                this.boardList = response.data.boardList;
-                //this.loading = false;
-                //this.linkMsg = response.data.testValue;
-                
-            }, (error)  =>  {
-                this.loading = false;
-            })
-
-          
+        }
+        ,
+        goDetail: function(No) {
+            alert(No);
+            this.$router.push({name: 'reply', params: { foo: "bar"}});
         }
     }
   }
