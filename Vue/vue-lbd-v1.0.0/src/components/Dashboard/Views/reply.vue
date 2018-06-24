e49119380a63ab177cf1c7dbfb8155ea2925fc39
yuhyunjae12 <yuhyunjae12@naver.com>
2018-06-24 22:08:37 +0900

    댓글 css 추가

--- Vue/vue-lbd-v1.0.0/src/components/Dashboard/Views/reply.vue
+++ Vue/vue-lbd-v1.0.0/src/components/Dashboard/Views/reply.vue
@@ -1,20 +1,44 @@
 <template>
-  <div class="content">
-    <div class="container-fluid">
+      <div class="container">
       <div class="row">
-        
-        <div id="app1" class="typo-line">
-        <input type="text" v-model="replyContent.content"/>
-        <button v-on:click="replyInsert">댓글작성</button>
-        <ul v-for="(reply,index) in replys" :key="reply.replyNo">
-            <li>{{ reply.replyNo }} , {{ reply.boardNo }} , {{ reply.content }} , {{ reply.replyGroup }} , {{ reply.useYn }} , <input type="button" value="댓글작성2" v-on:click="add(index, reply.replyNo)"/>
-             <div v-bind:id="'reply'+reply.replyNo"></div>
+         <div class="col-lg-5 offset-lg-4 col-12 comment-main rounded">
+          <ul class="p-0" v-for="reply in replys" :key="reply.replyNo">
+            <li>
+                <div class="row comment-box p-1 pt-3 pr-4">
+		        <div class="col-lg-2 col-3 user-img text-center">
+                    		{{ reply.replyNo }}
+                  	</div>
+                  <div class="col-lg-10 col-9 user-comment bg-light rounded pb-1">
+                       <div class="row">
+                             <div class="col-lg-8 col-6 border-bottom pr-0">
+                                <p class="w-100 p-2 m-0">{{ reply.content }}</p>
+                             </div>
+                             <div class="col-lg-4 col-6 border-bottom">
+                                <p class="w-100 p-2 m-0"><span class="float-right"><i class="fa fa-clock-o mr-1" aria-hidden="true"></i>01 : 00</span></p>
+                             </div>
+                       </div>
+                      <div class="user-comment-desc p-1 pl-2">
+                          <a class="m-0 mr-2" @click.prevent="replyInsertForm(reply.replyNo)">comment</a>
+                      </div>  
+                  </div>
+                </div>
             </li>
-        </ul>
+            <div class="test" v-bind:id="'reply'+reply.replyNo">
+            <br>
+            <div class="row">
+              <div class="col-lg-10 col-10">
+                <input type="text" class="form-control" placeholder="write comments ..." v-model="replyContent.content">
+              </div>
+              <div class="col-lg-2 col-2 send-icon">
+                <a href="#" @click.prevent="replyInsert" target="_blank"><i class="fa fa-paper-plane" aria-hidden="true"></i></a>
+              </div>
+            </div>
+            </div>
+            </ul>
         </div>
-      </div>
-    </div>
-  </div>
+        </div>
+        </div>
+  
 </template>
 
 <script>
@@ -59,18 +83,60 @@
 
          
        },
-       replyReset: function(id) {
-           var test =document.getElementById(id);
-           test.innerHTML="";
-       },
-       add: function(no, replyNo){
-           var aa = 'reply'+replyNo;
-           var test =document.getElementById(aa);
-           test.innerHTML="<input type='text'/><button onClick='replyReset("+aa+")'>취소</button><button>등록</button>";
+       replyInsertForm(replyNo){
+           var id = "reply"+replyNo;
+           var form =document.getElementById(id);
+           if (form.style.display === "none") {
+                form.style.display = "block";
+           } else {
+                form.style.display = "none";
+        }
        }
     }
   }
 </script>
 <style>
+.comment-main ul{
+	list-style: none;
+}
+.sub-cmt-img{
+	width: 55px !important;
+    height: 55px !important;
+    border-radius: 50%;
+} 
+.main-cmt-img{
+    width: 40px !important;
+    height: 40px !important;
+    border-radius: 50%;
+}
 
+.border-bottom{
+	font-size: 13px;
+	border-bottom: 1px solid #d3d3d3;
+}
+.user-comment{
+	background-color: #bae6e9 !important;
+    box-shadow: 0px 5px 8px -4px #c1c1c1;
+}
+.user-comment-desc{
+    font-size: 5px;
+	color: #a0a0a0;
+}
+.user-comment-desc p{
+	font-size: 12px;
+	display: inline-block;
+	/* float: left; */
+}
+.send-icon i{
+	font-size: 20px;
+	background: #d7eef0;
+	padding: 6px 5px;
+	border-radius: 50%;
+	color: #74C2E1;
+	height: 35px;
+	width: 35px;
+}
+.test{
+    display:none;
+}
 </style>
